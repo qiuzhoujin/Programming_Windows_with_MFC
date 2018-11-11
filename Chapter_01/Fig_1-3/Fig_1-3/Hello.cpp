9a66183c40c1123983dfe636beae32acaa09d209
Humphrey.Zhu <zhufang1987@yeah.net>
2018-11-11 11:06:40 +0800

    测试结构体数组的常规初始化方法。

--- Chapter_01/Fig_1-3/Fig_1-3/Hello.cpp
+++ Chapter_01/Fig_1-3/Fig_1-3/Hello.cpp
@@ -41,6 +41,6 @@ void CMainWindow::OnPaint()
 	dc.DrawText(str, -1, &rect,
 		DT_SINGLELINE | DT_CENTER | DT_VCENTER);
 
-	POINT aPoint[5] = {0, 0, 0, 100, 100, 100, 100, 0, 0, 0};
+	POINT aPoint[5] = {{0, 0}, {0, 100}, {100, 100}, {100, 0}, {0, 0}};
 	dc.Polyline(aPoint, 5);
 }
