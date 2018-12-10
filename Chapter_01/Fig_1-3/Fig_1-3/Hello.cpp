8b5741d6b97515bb93f574e67ef9389cf6b39161
Humphrey.Zhu <zhufang1987@yeah.net>
2018-12-10 20:10:40 +0800

    测试CDC类的TextOut。

--- Chapter_01/Fig_1-3/Fig_1-3/Hello.cpp
+++ Chapter_01/Fig_1-3/Fig_1-3/Hello.cpp
@@ -28,17 +28,6 @@ CMainWindow::CMainWindow()
 void CMainWindow::OnPaint()
 {
 	CPaintDC dc(this);
-	static int x1 = 104, y1 = 100;
-	static int x2 = x1 + 150, y2 = y1 + 100;
-
-	CBrush brush(HS_DIAGCROSS, RGB(255, 255, 255));
-	CPoint point(x1, y1);
-	dc.LPtoDP(&point);
-	point.x %= 8;
-	point.y %= 8;
-	brush.UnrealizeObject();
-	dc.SetBrushOrg(point);
-	dc.SelectObject(&brush);
-	dc.SetBkColor(RGB(192, 192, 192));
-	dc.Rectangle(x1, y1, x2, y2);
+	
+	dc.TextOut(100, 100, TEXT("lame"));
 }
